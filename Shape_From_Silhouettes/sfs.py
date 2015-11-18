dbc03397f34598e50c755f4c4a66d5b91c8a2235
Shawn Luo <shawn.luotianyu@gmail.com>
2015-11-18 17:58:49 -0500

    use mean to render

--- Shape_From_Silhouettes/sfs.py
+++ Shape_From_Silhouettes/sfs.py
@@ -54,8 +54,6 @@ def identify_surface(voxels, scale, xRange, yRange, zRange):
                     neighbor += voxels[x][y][z-1]
                 if z+1 < zRange*scale:
                     neighbor += voxels[x][y][z+1]
-                #if x-1 >= 0 and x+1 < xRange*scale and y-1 >= 0 and y+1 < yRange*scale and z-1>=0 and z+1 < zRange*scale:
-                    #neighbor = voxels[x-1][y][z]+voxels[x][y-1][z]+voxels[x][y][z-1]+voxels[x+1][y][z]+voxels[x][y+1][z]+voxels[x][y][z+1]
                 if voxels[x][y][z] == 0 and neighbor > 0:
                     surface[x][y][z] = 2
                     num += 1
@@ -63,17 +61,18 @@ def identify_surface(voxels, scale, xRange, yRange, zRange):
     return surface
 
 
-def construct_voxels(p_list, im_list, scale=20, xRange=5, yRange=6, zRange=2.5, h=582, w=780):
+def construct_voxels(p_list, im_list, scale=30, xRange=5, yRange=6, zRange=2.5, h=582, w=780):
     # construct voxels,(x,y,z)
     # all set to 0, means that at first, all inlier
     # if set value to 1, means that outlier, remove.
     voxels = np.zeros((xRange*scale, yRange*scale, zRange*scale))
-    #omegas = np.zeros((xRange*scale, yRange*scale, zRange*scale))
+    nearest_camera = np.zeros((xRange*scale, yRange*scale, zRange*scale))
 
     xOffset_global = int(xRange*scale/2)
     yOffset_global = int(yRange*scale/2)
     xOffset_image = int(w/2)
     yOffset_image = int(h/2)
+
     for index in range(0,8):
         count = 0
         for x in range(0, int(xRange*scale)):
@@ -91,8 +90,6 @@ def construct_voxels(p_list, im_list, scale=20, xRange=5, yRange=6, zRange=2.5, 
                     omega = raw_im_coord[2]
                     im_x = int(raw_im_coord[0]/omega)
                     im_y = int(raw_im_coord[1]/omega)
-                    #im_x = int(raw_im_coord[0]/raw_im_coord[2] + xOffset_image)
-                    #im_y = int(-raw_im_coord[1]/raw_im_coord[2] + yOffset_image)
                     im_coord = np.array([im_x, im_y])
                     #print(im_coord)
                     # 1, out of boundary, remove.
@@ -119,13 +116,82 @@ def construct_voxels(p_list, im_list, scale=20, xRange=5, yRange=6, zRange=2.5, 
             for z in range(0, int(zRange*scale)):
                 if surface[x][y][z] != 0:
                     voxels[x][y][z] = 2
-                if voxels[x][y][z] == 0:
-                    result_list.append((x,y,z,255,0,0))
+                #if voxels[x][y][z] == 0:
+                    #result_list.append((x,y,z,255,0,0))
                 if voxels[x][y][z] == 2:
                     result_list.append((x,y,z,255,255,255))
-    #result = np.asarray(result_list)
-    print(len(result_list))
     return result_list
+
+def texture_render(vertex_list, p_list, tex_list, scale=10, xRange=5, yRange=6):
+    rgb = []
+    nearest_camera = []
+    index = 0
+    xOffset = int(xRange*scale/2)
+    yOffset = int(yRange*scale/2)
+    result = []
+
+    for vertex in vertex_list:
+        min_depth = 65535
+        gx = (vertex[0]-xOffset)/scale
+        gy = (vertex[1]-yOffset)/scale
+        gz = vertex[2]/scale
+        global_coord = np.array([[gx, gy, gz, 1]])
+        xCoord = 0
+        yCoord = 0
+        # Strategy 1, nearest camera
+#        camera = 0
+#        for i in range(0,8):
+#            raw_im_coord = np.dot(p_list[i], global_coord.T)
+#            depth = raw_im_coord[2]
+#            im_x = int(raw_im_coord[0]/depth)
+#            im_y = int(raw_im_coord[1]/depth)
+#            if depth < min_depth:
+#                min_depth = depth
+#                camera = i
+#                xCoord = im_x
+#                yCoord = im_y
+#        rgb = tex_list[camera][yCoord][xCoord]
+#        result.append((vertex[0], vertex[1], vertex[2], rgb[0], rgb[1], rgb[2]))
+
+        # Strategy 2, mean of all camera
+        r = 0
+        g = 0
+        b = 0
+        for i in range(0,8):
+            raw_im_coord = np.dot(p_list[i], global_coord.T)
+            depth = raw_im_coord[2]
+            im_x = int(raw_im_coord[0]/depth)
+            im_y = int(raw_im_coord[1]/depth)
+
+            r += tex_list[i][im_y][im_x][0]
+            g += tex_list[i][im_y][im_x][1]
+            b += tex_list[i][im_y][im_x][2]
+        result.append((vertex[0],vertex[1],vertex[2],int(r/8),int(g/8),int(b/8)))
+
+        # Strategy 3, intensity
+#        y = 0
+#        min_y = 0
+#        camera = 0
+#        for i in range(0,8):
+#            raw_im_coord = np.dot(p_list[i], global_coord.T)
+#            depth = raw_im_coord[2]
+#            im_x = int(raw_im_coord[0]/depth)
+#            im_y = int(raw_im_coord[1]/depth)
+#            y = 0.299*tex_list[i][im_y][im_x][0]+0.587*tex_list[i][im_y][im_x][1]+0.114*tex_list[i][im_y][im_x][2]
+#            print(y)
+#            if y > min_y:
+#                min_y = y
+#                camera = i
+#                xCoord = im_x
+#                yCoord = im_y
+#        rgb = tex_list[camera][yCoord][xCoord]
+#        result.append((vertex[0], vertex[1], vertex[2], rgb[0], rgb[1], rgb[2]))
+
+    #print(result)
+    return result
+
+
+
 
 if __name__ == '__main__':
     # read from XML files and stroed into P
@@ -135,16 +201,18 @@ if __name__ == '__main__':
     # load silhouettes to image list
     im_list = pbm2Im()
 
-    # generate png, nothing
-#    idx = 0
-#    for im in im_list:
-#        print(im)
-#        scipy.misc.imsave(str(idx).join(['./ref/','.png']), im)
-#        idx += 1
-
+    # load texture image
+    tex_list = []
+    for i in range(0,8):
+        path = str(i).join(['./dataset/cam0','_00023_0000008550.png'])
+        im = np.array(Image.open(path))
+        tex_list.append(im)
     # construct voxels,(x,y,z)
     data = construct_voxels(p_list, im_list)
 
-    # write ply
-    write2ply(data)
+    # RGB render
+    result = texture_render(data, p_list, tex_list, 30)
+    # write PLY
+    write2ply(result)
+
 
